392c91c067c4b6dfc8f426d3d48497bf5e519db5
91779 <91779@DESKTOP-1QQGEHD>
2025-06-17 18:48:42 +0530

    Add project files.

---
+++ LibraryManagmentSystem/Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LibraryManagmentSystem.Models; 
+
+namespace LibraryManagmentSystem.Controllers
+{
+    public class BookController : ApiController
+    {
+        public IEnumerable<Book> Get()
+        {
+            using(LibraryDBEntities db = new LibraryDBEntities())
+            {
+                return db.Books.ToList();
+            }
+        }
+
+        public Book Get(int id)
+        {
+            using (LibraryDBEntities db = new LibraryDBEntities())
+            {
+                return db.Books.FirstOrDefault(b => b.Id == id);
+            }
+        }
+    }
+}
